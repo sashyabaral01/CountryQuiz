96df3c15e1459551b7ca3ae38b03aa3f1fe22b18
AJTuttle <alexander.tuttle@uga.edu>
2021-03-27 23:27:00 -0400

    added a fragment for questions, also implemented a sliding tab layout based on a class example

---
+++ app/src/main/java/edu/uga/cs/countryquiz/Questions.java
@@ -0,0 +1,151 @@
+package edu.uga.cs.countryquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Questions extends AppCompatActivity {
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    ViewPager mViewPager;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        //toolbar.setTitle(mSectionsPagerAdapter.getPageTitle(0));
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+
+
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        tabLayout.setTabsFromPagerAdapter(mSectionsPagerAdapter);
+        tabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+    }
+
+    public void loadView(ImageView imageView, int resId, TextView textView, String description) {
+        imageView.setImageResource(resId);
+        textView.setText(description);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 6;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            int imageNum = position + 1;
+            return String.valueOf("Q" + imageNum);
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        private int mImageNum;
+        private TextView mTextView;
+        private ImageView mImageView;
+
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if (getArguments() != null) {
+                mImageNum = getArguments().getInt(ARG_SECTION_NUMBER);
+            } else {
+                mImageNum = -1;
+            }
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_question, container, false);
+            //mTextView = (TextView) rootView.findViewById(R.id.section_label);
+            //mImageView = (ImageView) rootView.findViewById(R.id.image_view);
+            return rootView;
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            //if (Questions.class.isInstance(getActivity())) {
+            //    final int resId = Questions.imageIds[mImageNum - 1];
+            //    final String description = imageDescriptions[mImageNum - 1];
+            //    ((SimpleTabsActivity) getActivity()).loadView(mImageView, resId, mTextView, description);
+            //}
+        }
+    }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_simple_tabs, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
